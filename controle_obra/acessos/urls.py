d89cfdd760fbede3e0c5343f517641e5f4ef8ebf
Gustavo Khalil <khalilmelo85@gmail.com>
2026-01-02 19:42:41 -0300

    Fix: Add 'registrar_entrada_saida' URL alias to acessos/urls.py
    This fix adds a new URL pattern with name='registrar_entrada_saida' that points to the lista_presenca_hoje view.
    
    This resolves the TemplateDoesNotExist error in base_site.html where the button was trying to use {% url 'registrar_entrada_saida' %} which didn't exist.
    
    Now both /acessos/hoje/ and /acessos/registrar/ point to the same presence list view.

--- controle_obra/acessos/urls.py
+++ controle_obra/acessos/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('hoje/', views.lista_presenca_hoje, name='lista_presenca_hoje'),
     path('relatorio/exportar/', views.exportar_relatorio_mensal, name='exportar_relatorio'),
+        path('registrar/', views.lista_presenca_hoje, name='registrar_entrada_saida'),
 ]
+
