c7a3577185a4a638accc59425db74dd102f36fa5
Gustavo Khalil <khalilmelo85@gmail.com>
2026-01-02 21:23:17 -0300

    Add facial recognition fields to Funcionario model
    Added fields for facial recognition data and registration timestamp.

--- controle_obra/funcionarios/models.py
+++ controle_obra/funcionarios/models.py
@@ -8,5 +8,10 @@ class Funcionario(models.Model):
     telefone = models.CharField(max_length=20)
     foto = models.ImageField(upload_to='funcionarios/', null=True, blank=True)
 
+     facial_data = models.JSONField(null=True, blank=True, help_text='Dados de reconhecimento facial capturados da câmera')
+    facial_registered_at = models.DateTimeField(null=True, blank=True, help_text='Data/hora do registro facial')
+
+
     def __str__(self):
         return self.nome
+
