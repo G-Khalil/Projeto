3d19984dff8556d7171dc700baa64ffd0eb2e421
Gustavo Khalil <khalilmelo85@gmail.com>
2026-01-02 21:02:29 -0300

    Add route for lista_funcionarios_entrada_saida

--- controle_obra/acessos/urls.py
+++ controle_obra/acessos/urls.py
@@ -5,8 +5,10 @@ urlpatterns = [
     path('hoje/', views.lista_presenca_hoje, name='lista_presenca_hoje'),
     path('relatorio/exportar/', views.exportar_relatorio_mensal, name='exportar_relatorio'),
         path('registrar/', views.lista_presenca_hoje, name='registrar_entrada_saida'),
-    path('api/registrar-acesso/', views.registrar_acesso_ajax, name='registrar_acesso_ajax')
+    path('api/registrar-acesso/', views.registrar_acesso_ajax, name='registrar_acesso_ajax'),
+    path('lista-funcionarios/', views.lista_funcionarios_entrada_saida, name='lista_funcionarios_entrada_saida')
 ]
+
 
 
 
