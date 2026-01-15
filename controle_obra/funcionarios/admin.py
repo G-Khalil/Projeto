4e7c575dbfdbaa8f8e19f3562ef4275b615eee17
Gustavo Khalil <khalilmelo85@gmail.com>
2026-01-15 11:14:32 -0300

    Feat: Add search by name and filter by company in admin
    Added search fields, filters, and ordering to FuncionarioAdmin.

--- controle_obra/funcionarios/admin.py
+++ controle_obra/funcionarios/admin.py
@@ -1,17 +1,25 @@
 from django.contrib import admin
 from .models import Funcionario
 
-
 class FuncionarioAdmin(admin.ModelAdmin):
     list_display = ('nome', 'empresa', 'funcao', 'telefone', 'tem_foto', 'tem_facial_data')
     fields = ('nome', 'empresa', 'funcao', 'telefone', 'foto', 'facial_data', 'facial_registered_at')
     readonly_fields = ('facial_data', 'facial_registered_at')
-
+    
+    # BUSCA POR NOME
+    search_fields = ('nome', 'empresa__nome', 'funcao')
+    
+    # FILTRO POR EMPRESA
+    list_filter = ('empresa', 'funcao')
+    
+    # ORDENAÇÃO
+    ordering = ('empresa', 'nome')
+    
     def tem_foto(self, obj):
         return bool(obj.foto)
     tem_foto.boolean = True
     tem_foto.short_description = 'Tem Foto?'
-
+    
     def tem_facial_data(self, obj):
         if obj.facial_data:
             import json
@@ -24,6 +32,5 @@ class FuncionarioAdmin(admin.ModelAdmin):
         return False
     tem_facial_data.boolean = True
     tem_facial_data.short_description = 'Dados Faciais?'
-
 
 admin.site.register(Funcionario, FuncionarioAdmin)
