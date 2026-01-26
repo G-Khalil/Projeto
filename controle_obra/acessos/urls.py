0e6a9d9e35c7c6d500503d1fcb3a3793f9c845e3
Gustavo Khalil <khalilmelo85@gmail.com>
2026-01-26 11:48:03 -0300

    Add daily excel report url

--- controle_obra/acessos/urls.py
+++ controle_obra/acessos/urls.py
@@ -4,13 +4,10 @@ from . import views
 urlpatterns = [
     path('hoje/', views.lista_presenca_hoje, name='lista_presenca_hoje'),
     path('relatorio/exportar/', views.exportar_relatorio_mensal, name='exportar_relatorio'),
-        path('registrar/', views.lista_presenca_hoje, name='registrar_entrada_saida'),
+    path('relatorio/diario/exportar/', views.exportar_relatorio_diario, name='exportar_relatorio_diario'),
+    path('registrar/', views.registrar_entrada_saida, name='registrar_entrada_saida'),
     path('api/registrar-acesso/', views.registrar_acesso_ajax, name='registrar_acesso_ajax'),
     path('api/recognize-register/', views.recognize_and_register_ajax, name='recognize_and_register_ajax'),
     path('api/recognize-register-auto/', views.recognize_and_register_auto, name='recognize_and_register_auto'),
-    path('lista-funcionarios/', views.lista_funcionarios_entrada_saida, name='lista_funcionarios_entrada_saida')
+    path('lista-funcionarios/', views.lista_funcionarios_entrada_saida, name='lista_funcionarios_entrada_saida'),
 ]
-
-
-
-
