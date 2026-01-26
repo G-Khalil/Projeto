ffdce3c157313bac37b1dc559f13b7c935dc04f7
Gustavo Khalil <khalilmelo85@gmail.com>
2026-01-26 12:09:31 -0300

    Update AcessoObraAdmin to include get_empresa method

--- controle_obra/acessos/admin.py
+++ controle_obra/acessos/admin.py
@@ -3,7 +3,12 @@ from .models import AcessoObra
 
 @admin.register(AcessoObra)
 class AcessoObraAdmin(admin.ModelAdmin):
-    list_display = ('funcionario', 'empresa', 'data', 'hora_entrada', 'hora_saida')
-    list_filter = ('empresa', 'data', 'funcionario')
-    search_fields = ('funcionario__nome', 'empresa__nome')
-    ordering = ('empresa', 'data', 'funcionario')
+    list_display = ('funcionario', 'get_empresa', 'data', 'hora_entrada', 'hora_saida')
+    list_filter = ('funcionario__empresa', 'data', 'funcionario')
+    search_fields = ('funcionario__nome', 'funcionario__empresa__nome')
+    ordering = ('funcionario__empresa', 'data', 'funcionario')
+
+    def get_empresa(self, obj):
+        return obj.funcionario.empresa.nome if obj.funcionario and obj.funcionario.empresa else "N/A"
+    get_empresa.short_description = 'Empresa'
+    get_empresa.admin_order_field = 'funcionario__empresa'
