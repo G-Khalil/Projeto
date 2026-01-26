1d81825c36018e264df88ffc0cac2f69294a401b
Gustavo Khalil <khalilmelo85@gmail.com>
2026-01-26 11:36:00 -0300

    Enhance AcessoObraAdmin with additional fields

--- controle_obra/acessos/admin.py
+++ controle_obra/acessos/admin.py
@@ -3,5 +3,7 @@ from .models import AcessoObra
 
 @admin.register(AcessoObra)
 class AcessoObraAdmin(admin.ModelAdmin):
-    list_display = ('funcionario', 'data', 'hora_entrada', 'hora_saida')
-    list_filter = ('data', 'funcionario__empresa')
+    list_display = ('funcionario', 'empresa', 'data', 'hora_entrada', 'hora_saida')
+    list_filter = ('empresa', 'data', 'funcionario')
+    search_fields = ('funcionario__nome', 'empresa__nome')
+    ordering = ('empresa', 'data', 'funcionario')
