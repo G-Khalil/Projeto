6f80e262e9958dbb0f19aa3e98dffe681e55475f
Gustavo Khalil <khalilmelo85@gmail.com>
2026-01-26 13:37:46 -0300

    Fix indentation error in lista_presenca_hoje function
    Restored various functions related to attendance and reporting, including daily and monthly reports, facial recognition for entry/exit, and presence list by company.

--- controle_obra/acessos/views.py
+++ controle_obra/acessos/views.py
@@ -21,7 +21,7 @@ def lista_presenca_hoje(request):
     data_str = request.GET.get('data', timezone.now().strftime('%Y-%m-%d'))
     hoje = datetime.strptime(data_str, '%Y-%m-%d').date()
     dados_empresas = []
-        empresas = Empresa.objects.all().order_by('nome')
+    empresas = Empresa.objects.all().order_by('nome')
          
     for empresa in empresas:
 
@@ -311,6 +311,7 @@ def lista_funcionarios_entrada_saida(request):
             lista_final.append({'empresa': empresa.nome, 'funcionarios': funcs_status})
 
     return render(request, 'acessos/lista_funcionarios_entrada_saida.html', {'empresas_agrupadas': lista_final, 'data': hoje})
+
 
 
 
