9c17aded030b0b1dea46f478a468efd05536df48
Gustavo Khalil <khalilmelo85@gmail.com>
2026-01-27 14:24:34 -0300

    Fix urls.py - rename exportar_relatorio functions to exportar_excel

--- controle_obra/acessos/urls.py
+++ controle_obra/acessos/urls.py
@@ -3,14 +3,13 @@ from . import views
 
 urlpatterns = [
     path('hoje/', views.lista_presenca_hoje, name='lista_presenca_hoje'),
-    path('relatorio/exportar/', views.exportar_relatorio_mensal, name='exportar_relatorio'),
-    path('relatorio/diario/exportar/', views.exportar_relatorio_diario, name='exportar_relatorio_diario'),
+    path('relatorio/exportar/', views.exportar_excel_mensal, name='exportar_relatorio'),
+    path('relatorio/diario/exportar/', views.exportar_excel_diario, name='exportar_relatorio_diario'),
     path('registrar/', views.registrar_entrada_saida, name='registrar_entrada_saida'),
     path('api/registrar-acesso/', views.registrar_acesso_ajax, name='registrar_acesso_ajax'),
-    path('api/recognize-register/', views.recognize_and_register_ajax, name='recognize_and_register_ajax'),
-    path('api/recognize-register-auto/', views.recognize_and_register_auto, name='recognize_and_register_auto'),
+    path('api/reconhecer/', views.reconhecer_and_register_ajax, name='reconhecer_and_register_ajax'),
+    path('api/reconhecer-auto/', views.reconhecer_and_register_auto, name='reconhecer_and_register_auto'),
     path('lista-funcionarios/', views.lista_funcionarios_entrada_saida, name='lista_funcionarios_entrada_saida'),
-        path('exportar_excel_mensal/', views.exportar_excel_mensal, name='exportar_excel_mensal'),
-        path('exportar_excel_diario/', views.exportar_excel_diario, name='exportar_excel_diario'),
+    path('relatorio/mensal/exportar/', views.exportar_excel_mensal, name='exportar_excel_mensal'),
+    path('relatorio/diario/exportar/', views.exportar_excel_diario, name='exportar_excel_diario'),
 ]
-
